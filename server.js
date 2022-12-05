ff2ac8a6b9210c79736321ccac5962dc9c613afb
BS-GavinC <113683718+BS-GavinC@users.noreply.github.com>
2022-12-05 12:00:43 +0100

    Inputs

--- server.js
+++ server.js
@@ -20,11 +20,22 @@ app.get('/', (req, res) => {
             
         const request = new sql.Request()
 
-        request.query('SELECT * FROM student', (err, data) => {
+        request.input('id', sql.Int, 28);
+        request.input('firstname', sql.VarChar, "Paul")
+        request.input('lastname', sql.VarChar, "Briscard")
+        request.input('birthdate', sql.Date, new Date())
+        request.input('login', sql.VarChar, "pbriscard")
+        request.input('section', sql.Int, 1010)
+        request.input('result', sql.Int, 8)
+        request.input('course', sql.VarChar, "EG2210")
+
+        request.query('INSERT INTO student VALUES(@id, @firstname, @lastname, @birthdate, @login, @section, @result, @course)', (err, data) => {
             
             if(err) console.log(err);
 
             res.status(200).json(data.recordset)
+
+            
         })
     })
 
